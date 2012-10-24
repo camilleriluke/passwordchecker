994ba56f466b461e7e8317c33ea131631ca1e429
Luke Camilleri <camilleriluke@laptopisfar.(none)>
2012-10-24 22:46:22 +0200

    added some more code

--- passwordChecker.js
+++ passwordChecker.js
@@ -2,7 +2,7 @@
     // Place the script in strict mode
     'use strict';
 
-    /*global define, log*/
+    /*global define, log, document*/
     /*jslint plusplus: true */
 
     function PasswordComplexity() {}
@@ -12,14 +12,14 @@
         targetElement,
         // object with the plug-in options
         pluginOptions = {
-            text : {
-                title : "Password Strength meter",
-                invalid : "Invalid",
-                weak : "Weak",
-                strong : "String",
-                secure : "Secure"
+            text: {
+                title: "Password Strength meter",
+                invalid: "Invalid",
+                weak: "Weak",
+                strong: "String",
+                secure: "Secure"
             },
-            config : {
+            config: {
                 minLength: 0,
                 maxLength: 30
             }
@@ -30,22 +30,23 @@
         passChar,
         // the length of the password array
         passLength,
+        //
         passwordAnalytics = {
-            lower : 0,
-            upper : 0,
-            symbol : 0,
-            number : 0
+            lower: 0,
+            upper: 0,
+            symbol: 0,
+            number: 0
         },
         reusltCutOffPoints = {
-            WEAK : 33,
-            STRONG : 66,
-            SECURE : 99
+            WEAK: 33,
+            STRONG: 66,
+            SECURE: 99
         },
         regexTests = {
-            lower : /[a-z]/g,
-            upper : /[A-Z]/g,
-            numbers : /[0-9]/g,
-            symbols : /([_,#,@,%,\^,&,~,{,},(,),\-, ])/g
+            lower: /[a-z]/g,
+            upper: /[A-Z]/g,
+            numbers: /[0-9]/g,
+            symbols: /([_,#,@,%,\^,&,~,{,},(,),\-, ])/g
         };
 
     function analysePassword() {
@@ -110,6 +111,28 @@
 
     }
 
+    function createElementWithId(elementTag, id) {
+        var e = document.createElement(elementTag);
+        e.setAttribute('id', id);
+        return e;
+    }
+
+    function updatePluginElements() {
+        var template = '<div id="password-strength-direction" class="">' + '</div>' + '<div id="password-strength-direction2" class="">' + '</div>' + '<span id="title">' + 'Password Strength' + '</span>' + '<div id="meter-container">' + '<div id="meter-outer" style="display:block; width: 100%; height: 5px; background-color: #efefef; overflow: hidden;">' + '<span id="meter-inner" style="display:block; height: 100%;">' + '</span>' + '</div>' + '<span id="meter-strength">' + 'Invalid' + '</span>' + '</div>' + '<div>' + 'Password should:' + '<ul>' + '<li>' + '<span id="numbers" class="check-icon"></span>' + '<label for="numbers">' + 'have one number' + '</label>' + '</li>' + '<li>' + '<span id="lower" class="check-icon"></span>' + '<label for="lower">' + 'have one lowercase character' + '</label>' + '</li>' + '<li>' + '<span id="upper" class="check-icon"></span>' + '<label for="upper">' + 'have one uppercase character' + '</label>' + '</li>' + '<li>' + '<span id="length" class="check-icon"></span>' + '<label for="length">' + 'be between 6 and 30 characters' + '</label>' + '</li>' + '</ul>' + '<span style="font-size: small;">' + 'Note: Special characters allowed are: _ # % ^ &amp; ~ { } ( ) -' + '</span>' + '</div>',
+            e = createElementWithId('div', 'password-strength-meter');
+
+        e.innerHTML = template;
+
+        document.body.appendChild(e);
+        /*var direction1 = createElementWithId('div', 'password-strength-direction'),
+            direction2 = createElementWithId('div', 'password-strength-direction2'),
+            title = createElementWithId('span', 'title'),
+            meterContainer  = createElementWithId('div', 'meter-container'),
+            meterOuter = createElementWithId('div', 'meter-outer'),
+            meterInner = createElementWithId('span', 'meter-inner'),
+            meterInner = createElementWithId('span', 'meter-strength'),*/
+    }
+
     proto.bindTo = function bindTo(element, options) {
 
         if (!element) {
@@ -121,12 +144,12 @@
         passChar = passValue.split('');
         passLength = passChar.length;
 
+        updatePluginElements();
+
         bindEvent();
 
         resetPasswordAnalytics();
     };
-
-
 
 
 
